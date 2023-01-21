f0a1f4fc472c27f56ed0ca6c95e63001a6a955b3
Mario Fagundez <mario.fagundez@gmail.com>
2023-01-21 20:25:18 +0100

    add Date filter

--- README.md
+++ README.md
@@ -3,9 +3,14 @@ Timeline tweets exporter tool
 
 # usage
 Fill values in application.properties file with:
-* Your twitter api bearer token.
-* The username(s) you want to retrieve timeline.
-* The filepath where files will be generated (ending with / or \\)
+* bearer-token: Your twitter api bearer token.
+* usernames: The username(s) you want to retrieve the timeline, separated by comma.
+* filepath: The filepath where files will be generated (ending with / or \\).
+* startDatetime: (opt) Start date to extract tweets (Must be defined if end date is defined). yyyy-mm-ddThh:MM:ss
+* endDatetime: (opt) End date to extract tweets (Must be defined if start date is defined). yyyy-mm-ddThh:MM:ss
+
+java -jar twitter-exporter.jar
 
 # output
 All tweets from all provided usernames will be logged.
+
--- src/main/java/org/twitterexporter/TwitterExporterApplication.java
+++ src/main/java/org/twitterexporter/TwitterExporterApplication.java
@@ -1,6 +1,10 @@
 package org.twitterexporter;
 
 import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -45,6 +49,21 @@ public class TwitterExporterApplication {
 		// Create api client using bearer token from properties file
 		TwitterApi twitterApi = new TwitterApi(new TwitterCredentialsBearer(properties.getBearerToken()));
 
+		OffsetDateTime startDatetime = null;
+		OffsetDateTime endDatetime = null;
+
+		// Parsing start and end date if defined (none or both must be defined)
+		if ((properties.getStartDatetime() != null) && (!properties.getStartDatetime().isBlank()) && (properties.getEndDatetime() != null) && (!properties.getEndDatetime().isBlank())) {
+			startDatetime = LocalDateTime.parse(properties.getStartDatetime(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).atZone(ZoneId.systemDefault()).toOffsetDateTime();
+			endDatetime = LocalDateTime.parse(properties.getEndDatetime(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).atZone(ZoneId.systemDefault()).toOffsetDateTime();
+
+			// Start date (if defined) must be earlier than end date
+			if (!startDatetime.isBefore(endDatetime)){
+				log.error("StartDate ({}) is after than endDate ({}).", startDatetime, endDatetime);
+				throw new IllegalArgumentException();
+			}
+		}
+
 		// Each user has its own file
 		for(String username : properties.getUsernames()) {
 			try {
@@ -52,15 +71,26 @@ public class TwitterExporterApplication {
 				User userData = twitterApi.users().findUserByUsername(username).execute().getData();
 				if (userData != null) {
 					String userId = userData.getId();
+					// Generating file using username as filename
 					try (CSVWriter writer = new CSVWriter(new FileWriter(properties.getFilepath() + username + EXPORT_FILE_EXTENSION))){
 						// header row
 						writer.writeNext(new String[]{"tweetId", "creationDate", "conversationId", "editsRemaining", "language", "possiblySensitive", 
 						"likeCount", "quoteCount", "replyCount", "retweetCount", "replySettings", "text"});
+						if ((startDatetime != null) && (endDatetime != null)) {
+							log.info("Getting tweets from {} between {} and {}", username, startDatetime.toString(), endDatetime.toString());
+						} else {
+							log.info("Getting all tweets from {}", username);
+						}
 						
 						String nextToken = "";
 						while(nextToken != null) {
 							// Retrieve tweets from user, including RT.
 							APIusersIdTweetsRequest request = twitterApi.tweets().usersIdTweets(userId).maxResults(CHUNK_SIZE).tweetFields(TWEET_FIELDS);
+							// Include date filter if defined
+							if ((startDatetime != null) && (endDatetime != null)) {
+								request.startTime(startDatetime);
+								request.endTime(endDatetime);
+							}
 							if (!nextToken.isEmpty()) {
 								request.paginationToken(nextToken);
 							}
@@ -70,11 +100,17 @@ public class TwitterExporterApplication {
 							if (tweets != null) {
 								tweets.forEach(tweet -> {
 									log.debug(tweet.getText());
-									String flatText = (tweet.getText() != null) ? tweet.getText().replace("\n", " ") : null;
 									String creationDate = (tweet.getCreatedAt() != null) ? tweet.getCreatedAt().toString() : null;
 									String editsRemaining = (tweet.getEditControls() != null) ? tweet.getEditControls().getEditsRemaining().toString() : null;
 									String possibleSensitive = (tweet.getPossiblySensitive() != null) ? tweet.getPossiblySensitive().toString() : null;
+									String likeCount = ((tweet.getPublicMetrics() != null) && (tweet.getPublicMetrics().getLikeCount() != null)) ? tweet.getPublicMetrics().getLikeCount().toString() : null;
+									String quoteCount = ((tweet.getPublicMetrics() != null) && (tweet.getPublicMetrics().getQuoteCount() != null)) ? tweet.getPublicMetrics().getQuoteCount().toString() : null;
+									String replyCount = ((tweet.getPublicMetrics() != null) && (tweet.getPublicMetrics().getReplyCount() != null)) ? tweet.getPublicMetrics().getReplyCount().toString() : null;
+									String retweetCount = ((tweet.getPublicMetrics() != null) && (tweet.getPublicMetrics().getRetweetCount() != null)) ? tweet.getPublicMetrics().getRetweetCount().toString() : null;
+									String replySettings = (tweet.getReplySettings() != null) ? tweet.getReplySettings().toString() : null;
+									String flatText = (tweet.getText() != null) ? tweet.getText().replace("\n", " ") : null;
 
+									// Write file row with tweet info
 									writer.writeNext(new String[] {
 										tweet.getId(), 
 										creationDate,
@@ -82,19 +118,21 @@ public class TwitterExporterApplication {
 										editsRemaining, 
 										tweet.getLang(), 
 										possibleSensitive, 
-										tweet.getPublicMetrics().getLikeCount().toString(), 
-										tweet.getPublicMetrics().getQuoteCount().toString(), 
-										tweet.getPublicMetrics().getReplyCount().toString(), 
-										tweet.getPublicMetrics().getRetweetCount().toString(),
-										tweet.getReplySettings().toString(), 
+										likeCount, 
+										quoteCount, 
+										replyCount, 
+										retweetCount,
+										replySettings, 
 										flatText});
 								});
 							}
 
+							// Get next token to retrieve next chunk
 							Get2UsersIdMentionsResponseMeta meta = tweetsResponse.getMeta();
 							if (meta != null) {
 								nextToken = meta.getNextToken();
 							}
+							// Log each detected problem (if any)
 							List<Problem> problems = tweetsResponse.getErrors();
 							if (problems != null){
 								log.error("Detected errors wile retrieving data from Twitter API: {}", problems.toString());
